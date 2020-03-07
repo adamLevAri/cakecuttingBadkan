b3cf90a8cd9d7f77fde992aca897dc20b54d961a
adamLevAri <37263805+adamLevAri@users.noreply.github.com>
2020-03-08 01:15:50 +0200

    Update CutAndChoose.cpp

--- CutAndChoose.cpp
+++ CutAndChoose.cpp
@@ -7,7 +7,41 @@
 //
 
 #include "CutAndChoose.hpp"
+Allocation:: Allocation() {};
+    
+Allocation:: Allocation(vector<Agent*> agents) {
+        
+        this->agents = agents;
+        (this->pieces).resize(agents.size());
+};
+    
+Allocation:: ~Allocation() {};
+    
+//TODO: sets the piece of the given index
+void Allocation:: setPiece(int agentIndex, vector<float> piece){
 
+        this->pieces[agentIndex] = piece;
+        
+};
+    
+string Allocation:: toString(vector<float> vec){
+        ostringstream os;
+        os << "[" << vec[0] << ", " << vec[1] << "]" << endl;
+        return os.str();
+        
+};
+    
+string Allocation:: print(){
+
+        ostringstream os;
+        for (int i=0; i<pieces.size(); i++) {
+            os << agents[i]->getName() << " gets " << toString(pieces[i])
+            << " with value " << agents[i]->pieceValue(pieces[i]) << endl;
+    }
+        
+    return os.str();
+        
+};
 namespace cake {
 
     Allocation asymmetric_protocol(vector<Agent*> agents) {
